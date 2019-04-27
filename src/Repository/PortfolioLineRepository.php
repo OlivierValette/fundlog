ed5fb7c3791755853d96737cb2ffa6ce8cc73a6e
Olivier Valette <olivier.valette@spi10.com>
2019-04-27 17:01:31 +0200

    Adapted findIoLines() with new ioConfirm property

--- src/Repository/PortfolioLineRepository.php
+++ src/Repository/PortfolioLineRepository.php
@@ -19,6 +19,11 @@ class PortfolioLineRepository extends ServiceEntityRepository
         parent::__construct($registry, PortfolioLine::class);
     }
     
+    /** findActiveLines : retrieve real lines of portfolio (with qty > 0)
+     *                    (other lines are new lines in a transaction before confirmation)
+     * @param $pf       Portfolio object
+     * @return array
+     */
     public function findActiveLines($pf): array
     {
     
@@ -36,6 +41,12 @@ class PortfolioLineRepository extends ServiceEntityRepository
         return $qb->getQuery()->getResult();
     }
     
+    /** findIoLines : retrieve lines concerned by a transaction
+     *                - lines with pending transaction (ioValue != 0.0)
+     *                - lines confirmed (thus with ioValue = 0.0 ) but whole transaction not confirmed
+     * @param $pf       Portfolio object
+     * @return array
+     */
     public function findIoLines($pf): array
     {
         
@@ -43,17 +54,27 @@ class PortfolioLineRepository extends ServiceEntityRepository
         $qb = $this->createQueryBuilder('pfl');
         $qb = $qb->select('pfl')
             ->andWhere($qb->expr()->eq('pfl.portfolio', ':pf'))
-            ->andWhere($qb->expr()->neq('pfl.ioValue', ':val'))
+            ->andWhere($qb->expr()->orX(
+                $qb->expr()->neq('pfl.ioValue', ':value'),
+                $qb->expr()->eq('pfl.ioConfirm', ':confirm')
+                )
+            )
             ->orderBy('pfl.id', 'ASC');
         
         // Give parameters values
         $qb->setParameter(':pf', $pf);
-        $qb->setParameter(':val', 0.0);
+        $qb->setParameter(':value', 0.0);
+        $qb->setParameter(':confirm', true);
         
         return $qb->getQuery()->getResult();
     }
     
     
+    /** ioTotalAmount : compute the total amount of a transaction
+     * @param $pf       Portfolio object
+     * @return float
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
     public function ioTotalAmount($pf): float
     {
         // Create query
