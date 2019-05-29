f884688b9449b7a4594839dde905a256667217ef
Olivier Valette <olivier.valette@spi10.com>
2019-05-29 14:23:19 +0200

    PortfolioLine CRUD cleaning

--- src/Controller/PortfolioLineController.php
+++ src/Controller/PortfolioLineController.php
@@ -17,53 +17,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class PortfolioLineController extends AbstractController
 {
     /**
-     * @Route("/", name="portfolio_line_index", methods={"GET"})
-     */
-    public function index(): Response
-    {
-        $portfolioLines = $this->getDoctrine()
-            ->getRepository(PortfolioLine::class)
-            ->findAll();
-
-        return $this->render('portfolio_line/index.html.twig', [
-            'portfolio_lines' => $portfolioLines,
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="portfolio_line_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $portfolioLine = new PortfolioLine();
-        $form = $this->createForm(PortfolioLineType::class, $portfolioLine);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($portfolioLine);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('portfolio_line_index');
-        }
-
-        return $this->render('portfolio_line/new.html.twig', [
-            'portfolio_line' => $portfolioLine,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="portfolio_line_show", methods={"GET"})
-     */
-    public function show(PortfolioLine $portfolioLine): Response
-    {
-        return $this->render('portfolio_line/show.html.twig', [
-            'portfolio_line' => $portfolioLine,
-        ]);
-    }
-
-    /**
      * @Route("/{id}/edit", name="portfolio_line_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, PortfolioLine $portfolioLine): Response
@@ -73,9 +26,9 @@ class PortfolioLineController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('portfolio_line_index', [
-                'id' => $portfolioLine->getId(),
+    
+            return $this->redirectToRoute('portfolio_edit', [
+                'id' => $portfolioLine->getPortfolio()->getId()
             ]);
         }
 
