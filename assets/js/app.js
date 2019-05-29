9dc42d25f126d20d95d27766d67ac5abf61f9c55
Olivier Valette <olivier.valette@spi10.com>
2019-05-29 11:09:05 +0200

    Duplicating table search

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,20 +34,25 @@ $(document).ready(function() {
     });
 
     // Search in table
-    $("#fundBaseSearch").keyup(function() {
-        var value = this.value.toLowerCase().trim();
-
-        $("table tr").each(function (index) {
-            if (!index) return;
-            $(this).find("td").each(function () {
-                var id = $(this).text().toLowerCase().trim();
-                var not_found = (id.indexOf(value) == -1);
-                $(this).closest('tr').toggle(!not_found);
-                return not_found;
+    function filterTable(inputId, targetId) {
+        $(inputId).keyup(function() {
+            var value = this.value.toLowerCase().trim();
+            var target = targetId + " tr";
+            $(target).each(function (index) {
+                if (!index) return;
+                $(this).find("td").each(function () {
+                    var id = $(this).text().toLowerCase().trim();
+                    var not_found = (id.indexOf(value) == -1);
+                    $(this).closest('tr').toggle(!not_found);
+                    return not_found;
+                });
             });
         });
-    });
+    }
 
+    filterTable("#fundBaseSearch", "#fundBaseTable");
+    filterTable("#fundListSearch", "#fundListTable");
+    filterTable("#pfIoSearch", "#pfIoTable");
 
     // Modal for portfolio lines editing
 
